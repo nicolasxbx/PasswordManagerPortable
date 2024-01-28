1bac6088961ef4c81fb97e6a4c75fea7b298a6e6
nicolasbinder88@gmail.com <nicolasbinder88@gmail.com>
2024-01-28 16:55:31 +0100

    Projektdateien hinzufügen.

---
+++ Core/Models/Entry.cs
@@ -0,0 +1,18 @@
+﻿namespace Core;
+
+public record Entry
+{
+    public DateTime AddedUTC { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public List<string> URLs { get; set; } = new();
+    public List<string> Tags { get; set; } = new();
+    public string Note { get; set; } = string.Empty;    
+}
+
+public class Group
+{
+
+}
